7a0d779c5bde01731f73d7697cd530c24c57b2cb
Erwann Mest <erwann.mest@gmail.com>
2013-07-26 11:51:07 +0200

    Fix twitter redirect.

--- handlers/twitter.js
+++ handlers/twitter.js
@@ -3,6 +3,33 @@ var https = require('https'),
 
 exports.pattern = 'https://twitter.com/';
 
+function getContent(url, context) {
+  https.get(url, function(response) {
+    if(response.statusCode === 302) {
+      getContent(response.headers.location, context);
+    }
+    else {
+      var dom = '';
+
+      response.on("data", function(chunk) {
+        dom += chunk;
+      });
+
+      response.on("end", function() {
+        dom = dom.toString();
+
+        $ = cheerio.load(dom);
+
+        var $tweet = $('.opened-tweet').find('.js-tweet-text').first(),
+            $author = $('.permalink-tweet-container').first().find('.js-action-profile-name b').first(),
+            tweet = '@' + $author.text() + ': ' + $tweet.text();
+
+        context.client.say(context.to, tweet);
+      });
+    }
+  });
+}
+
 exports.callback = function(context) {
   var pattern = /https?:\/\/\S+/g,
       getUrl = new RegExp(pattern),
@@ -10,24 +37,6 @@ exports.callback = function(context) {
 
   url = context.message.match(getUrl)[0];
 
-  https.get(url, function(response) {
-    var dom = '';
-
-    response.on("data", function(chunk) {
-      dom += chunk;
-    });
-
-    response.on("end", function() {
-      dom = dom.toString();
-
-      $ = cheerio.load(dom);
-
-      var $tweet = $('.opened-tweet').find('.js-tweet-text').first(),
-          $author = $('.permalink-tweet-container').first().find('.js-action-profile-name b').first(),
-          tweet = '@' + $author.text() + ': ' + $tweet.text();
-
-      context.client.say(context.to, tweet);
-    });
-  });
+  getContent(url, context);
 }
 
