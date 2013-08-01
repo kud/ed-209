7201f9783afe1460a9e3d7dad43db1f941f913ab
Erwann Mest <erwann.mest@gmail.com>
2013-08-02 01:06:43 +0200

    Better handler for twitter.

--- listeners/twitter.js
+++ listeners/twitter.js
@@ -1,112 +1,110 @@
+/**
+ * Listener: Twitter
+ */
+
+// Imports
 var https = require('https'),
     cheerio = require('cheerio');
 
-(function(listener) {
-  listener.matcher = function(message, envelope) {
-    return (envelope.type == 'channel') &&
-           message.search('https://twitter.com') !== -1;
-  }
+var twitter = {
+  parseURL: function(url, callback) {
+    var mode = 'bio',
+        self = this;
 
-  listener.callback = function(message, envelope) {
+    if(url.search(/photo/) > 0) {
+      mode = 'photo';
+    }
+
+    if(url.search(/status/) > 0 && mode === 'bio') {
+      mode = 'status';
+    }
+
+    // call
+    https.get(url, function(response) {
+      // redirection?
+      if(response.statusCode === 302) {
+        self.parseURL(response.headers.location, callback);
+      }
+      else {
+        var dom = '';
+
+        response.on("data", function(chunk) {
+          dom += chunk;
+        });
+
+        response.on("end", function() {
+          dom = dom.toString();
+          var $ = cheerio.load(dom),
+
+              author = '',
+              tweet = '';
+
+           // photo
+          if(mode === 'photo') {
+            var $tweet = $('.tweet').find('.tweet-text').first(),
+                $author = $('.tweet').find('.tweet-screen-name').first(),
+                $imgUrl = $('.media-gallery-image-wrapper').find('.media-slideshow-image').first(),
+                imgUrl = 'Photo: ' + $imgUrl.attr('src');
+
+            tweet = [$tweet.text(), imgUrl].join('\n');
+            author = $author.text();
+          }
+
+           // status
+          if(mode === 'status') {
+            var $tweet = $('.opened-tweet').find('.js-tweet-text').first(),
+                $author = $('.permalink-tweet-container').first().find('.js-action-profile-name b').first();
+
+            tweet = $tweet.text();
+            author = $author.text();
+          }
+
+          // bio
+          if(mode === 'bio') {
+            var $tweet = $('.profile-header').first().find('.bio-container .bio').first(),
+                $author = $('.profile-header').first().find('.screen-name').first();
+
+            tweet = $tweet.text();
+            author = $author.text();
+            author = author.substring(1);
+          }
+
+          if(tweet.length === 0) {
+            tweet = 'No tweet available.'
+          }
+          else {
+            tweet = '@' + author + ': ' + tweet;
+
+            // clean the tweet
+            tweet = tweet.replace(/\:large/gi, "");
+            tweet = tweet.replace(/pic\.twitter\.com/gi, "http://pic.twitter.com");
+          }
+
+          callback(tweet);
+        });
+      }
+    });
+  }
+}
+
+var listener = {
+  matcher: function(message, envelope) {
+    return (envelope.type == 'channel') && message.search('https://twitter.com') !== -1;
+  },
+
+  callback: function(message, envelope) {
     var pattern = /https?:\/\/\S+/g,
         getUrl = new RegExp(pattern),
-        url,
+        url = undefined,
         self = this;
 
     url = message.match(getUrl)[0];
 
-    parseURL(url, function(result) {
+    twitter.parseURL(url, function(result) {
       self.reply(envelope, result);
     });
   }
+}
 
-  function parseURL(url, callback) {
-
-    // photo
-    if(url.search(/photo/) > 0) {
-        https.get(url, function(response) {
-        var dom = '';
-
-        response.on("data", function(chunk) {
-          dom += chunk;
-        });
-
-        response.on("end", function() {
-          dom = dom.toString();
-
-          $ = cheerio.load(dom);
-
-          var $tweet = $('.tweet').find('.tweet-text').first(),
-              $author = $('.tweet').find('.tweet-screen-name').first(),
-              $imgUrl = $('.media-gallery-image-wrapper').find('.media-slideshow-image').first();
-              tweet = '@' + $author.text() + ': ' + $tweet.text(),
-              imgUrl = $imgUrl.attr('src');
-
-          var reTweet = /pic\.twitter\.com/gi;
-
-          tweet = tweet.replace(reTweet, "http://pic.twitter.com");
-
-          var reImgUrl = /\:large/gi;
-
-          imgUrl = imgUrl.replace(reImgUrl, "");
-
-          callback([tweet, imgUrl].join('\n'));
-        });
-
-      });
-    }
-
-    // tweet
-    else if(url.search(/status/) > 0) {
-      https.get(url, function(response) {
-        if(response.statusCode === 302) {
-          parseURL(response.headers.location, callback);
-        }
-        else {
-          var dom = '';
-
-          response.on("data", function(chunk) {
-            dom += chunk;
-          });
-
-          response.on("end", function() {
-            dom = dom.toString();
-
-            $ = cheerio.load(dom);
-
-            var $tweet = $('.opened-tweet').find('.js-tweet-text').first(),
-              $author = $('.permalink-tweet-container').first().find('.js-action-profile-name b').first(),
-              tweet = '@' + $author.text() + ': ' + $tweet.text();
-
-            callback(tweet);
-          });
-        }
-      });
-    }
-
-    // bio
-    else {
-      https.get(url, function(response) {
-        var dom = '';
-
-        response.on("data", function(chunk) {
-          dom += chunk;
-        });
-
-        response.on("end", function() {
-          dom = dom.toString();
-
-          $ = cheerio.load(dom);
-
-          var $tweet = $('.profile-header').first().find('.bio-container .bio').first(),
-              $author = $('.profile-header').first().find('.screen-name').first(),
-              tweet = $author.text() + ': ' + $tweet.text();
-
-          callback(tweet);
-        });
-      });
-    }
-  }
-
-})(exports);
+module.exports = listener;
 
