592b85c9c3079168266a2e61cf533dfe40410a0e
madx <madx+github@yapok.org>
2013-08-02 08:33:32 +0200

    Add notice handling to the logger listener

--- listeners/logger.js
+++ listeners/logger.js
@@ -1,7 +1,14 @@
 exports.matcher = function(message, envelope) {
-  console.log(envelope.from +
-              (envelope.type == 'channel' ? '['+envelope.to+']' : '') +
-              ': ' +
-              message);
+  console.log([
+    envelope.type == 'notice' ? '-- ' : '',
+    envelope.from !== undefined ? envelope.from : '<server>',
+    (
+      (envelope.type == 'channel' || envelope.type == 'notice')
+      ? ' ['+envelope.to+']'
+      : ''
+    ),
+    ': ',
+    message
+  ].join(''));
   return false;
 }
