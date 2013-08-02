e7efc84070e205a5bf2c750e97be6d230f589ab0
madx <madx+github@yapok.org>
2013-08-02 15:30:33 +0200

    js: Secure setTimeout

--- listeners/js.js
+++ listeners/js.js
@@ -24,6 +24,11 @@
         if (str.match(/\bif\b|\bwhile\b|\bfor\b/) !== null) {
           return "Shove it up your ass, fucker"
         }
+        if (str.match(/\bsetTimeout\b/) !== null) {
+          var timeout = 1000 *  Math.floor((Math.random() * 10));
+          setTimeout(function() { reply('SURPRISE, MOTHER FUCKER!') }, timeout);
+          return "Yeah, like I'm going to eval that"
+        }
         if (str.match(/\bsetInterval\b/) !== null) {
           var makeCallback = function(tries) {
             return function() {
