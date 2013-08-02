96e1bcf5c3fa111bc39d8715689c4afc76b0ed25
madx <madx+github@yapok.org>
2013-08-02 08:45:36 +0200

    Add the autoreload feature

---
+++ listeners/autoreload.js
@@ -0,0 +1,22 @@
+var spawn = require('child_process').spawn;
+
+var listener = {
+  matcher: function(message, envelope) {
+    return envelope.type == 'notice'       &&
+           envelope.from !== undefined     &&
+           message.search('pushed') !== -1 &&
+           message.search('git.io') !== -1
+  },
+
+  callback: function(message, envelope) {
+    var git = spawn('git', ['pull'])
+        self = this;
+
+    git.on('close', function(code) {
+      console.log('git pull finished: '+code);
+      self.reply(envelope, 'Changes pulled');
+    });
+  }
+}
+
+module.exports = listener;
