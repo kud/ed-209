f17acbefb3fa14c0267051e229e96bda4d94f7e6
Erwann Mest <erwann.mest@gmail.com>
2013-10-17 15:13:29 +0200

    Add some words for sorry.

--- listeners/sorry.js
+++ listeners/sorry.js
@@ -1,7 +1,11 @@
 ;(function(listener) {
   listener.matcher = function(message, envelope) {
     return (envelope.type == 'channel') &&
-           message.search('sorry') !== -1
+           (message.search('sorry') !== -1 ||
+           message.search('désolé') !== -1 ||
+           message.search('oups') !== -1 ||
+           message.search('my bad') !== -1 ||
+           message.search('oops') !== -1)
   }
 
   listener.callback = function(message, envelope) {
