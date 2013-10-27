25b390903a4ad7be3a9060da412c6ca4c5387efe
madx <madx+github@yapok.org>
2013-10-27 06:45:53 +0100

    oups: Case insensitive matching

--- listeners/sorry.js
+++ listeners/sorry.js
@@ -1,11 +1,11 @@
 ;(function(listener) {
   listener.matcher = function(message, envelope) {
     return (envelope.type == 'channel') &&
-           (message.match(/\bsorry\b/) !== null ||
-           message.match(/\bdésolé\b/) !== null ||
-           message.match(/\boups\b/)   !== null ||
-           message.match(/\bmy bad\b/) !== null ||
-           message.match(/\boops\b/)   !== null)
+           (message.match(/\bsorry\b/i) !== null ||
+           message.match(/\bdésolé\b/i) !== null ||
+           message.match(/\boups\b/i)   !== null ||
+           message.match(/\bmy bad\b/i) !== null ||
+           message.match(/\boops\b/i)   !== null)
   }
 
   listener.callback = function(message, envelope) {
