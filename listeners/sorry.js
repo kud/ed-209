2bb4caefcc82d10d74d847677ce159434b42ed95
madx <madx+github@yapok.org>
2013-11-13 15:43:06 +0100

    sorry: Delimit with spaces or string start/end for less verbosity

--- listeners/sorry.js
+++ listeners/sorry.js
@@ -1,11 +1,11 @@
 ;(function(listener) {
   listener.matcher = function(message, envelope) {
     return (envelope.type == 'channel') &&
-           (message.match(/\bsorry\b/i) !== null ||
-           message.match(/\bdésolé\b/i) !== null ||
-           message.match(/\boups\b/i)   !== null ||
-           message.match(/\bmy bad\b/i) !== null ||
-           message.match(/\boops\b/i)   !== null)
+           (message.match(/(^| )sorry($| )/i) !== null ||
+           message.match(/(^| )désolé($| )/i) !== null ||
+           message.match(/(^| )oups($| )/i)   !== null ||
+           message.match(/(^| )my bad($| )/i) !== null ||
+           message.match(/(^| )oops($| )/i)   !== null)
   }
 
   listener.callback = function(message, envelope) {
