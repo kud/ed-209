9ee115bc5aa8d12a07a588680d001b1d571b67ad
Erwann Mest <m+github@kud.io>
2014-02-06 15:23:19 +0100

    Add thanks. Also ease the installation and the start.

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 install:
-	npm install
+	@ echo > Installing...
+	@ npm install
+	@ cp -i config.json.dist config.json
+
+start:
+	@ echo > Starting...
+	@ nodemon index.js
---
+++ listeners/thanks.js
@@ -0,0 +1,13 @@
+;(function(listener) {
+  listener.matcher = function(message, envelope) {
+    var regEx = 'merci ' + this.client.opt.nick
+
+    return (envelope.type == 'channel') &&
+           (message.match(regEx, 'i' !== null))
+  }
+
+  listener.callback = function(message, envelope) {
+    this.reply(envelope, 'De rine.')
+  }
+
+})(exports)
