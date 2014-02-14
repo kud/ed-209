6c456f4fc86100d66b4414d860035d76f0dbd8af
Maxime Thirouin <m@moox.io>
2014-02-14 14:10:48 +0100

    Update sorry.js

--- listeners/sorry.js
+++ listeners/sorry.js
@@ -2,10 +2,13 @@
   listener.matcher = function(message, envelope) {
     return (envelope.type == 'channel') &&
            (message.match(/(^| )sorry($| )/i) !== null ||
+           message.match(/(^| )sry($| )/i) !== null ||
            message.match(/(^| )désolé($| )/i) !== null ||
+           message.match(/(^| )desole($| )/i) !== null ||
+           message.match(/(^| )dsl($| )/i) !== null ||
            message.match(/(^| )oups($| )/i)   !== null ||
-           message.match(/(^| )my bad($| )/i) !== null ||
-           message.match(/(^| )oops($| )/i)   !== null)
+           message.match(/(^| )oops($| )/i)   !== null ||
+           message.match(/(^| )my bad($| )/i) !== null)
   }
 
   listener.callback = function(message, envelope) {
