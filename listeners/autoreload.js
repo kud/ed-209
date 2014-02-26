4735596bb769943fe64eb752f1d022031f6bbb9b
François Vaux <madx+github@yapok.org>
2014-02-26 10:06:17 +0100

    autoreload: Change reload conditions

--- listeners/autoreload.js
+++ listeners/autoreload.js
@@ -6,10 +6,10 @@ var spawn = require('child_process').spawn;
 
 var listener = {
   matcher: function(message, envelope) {
-    return envelope.type == 'notice'       &&
-           envelope.from !== undefined     &&
-           message.search('pushed') !== -1 &&
-           message.search('master') !== -1 &&
+    return envelope.from == 'putaindebot'        &&
+           message.search('\\[ed-209\\]') !== -1 &&
+           message.search('pushed') !== -1       &&
+           message.search('master') !== -1       &&
            message.search('git.io') !== -1
   },
 
