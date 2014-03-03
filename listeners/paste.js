dd613d860518e9295469e101cb1302dd139e33a4
Erwann Mest <m+github@kud.io>
2014-03-03 12:01:47 +0100

    Add kick on paste.

--- listeners/paste.js
+++ listeners/paste.js
@@ -7,18 +7,30 @@
   }
 
   listener.callback = function(message, envelope) {
-    var args = this.util.extractParams(message, 'roll')
-      , speech
+    var args = this.util.extractParams(message, 'paste')
+      , speech = "https://gist.github.com/"
+      , pseudo = typeof args[0] !== "undefined" ? args[0] : false
+      , kick = typeof args[1] !== "undefined" && args[1] === "kick" ? true : false
 
-    if ( typeof args[2] !== "undefined" ) {
-      speech = args[2] + ": will you paste your code on https://gist.github.com/ for fuck's sake!"
+  console.log(args)
+  console.log(typeof args[1] !== "undefined")
+  console.log(args[1])
+  console.log(envelope)
+  console.log(envelope.to)
+
+
+    // kick mode
+    if ( pseudo && kick ) {
+      this.client.send('KICK', envelope.to, pseudo, "Will you paste your code on https://gist.github.com/ for fuck's sake!")
     }
 
-    else {
-      speech = 'https://gist.github.com/'
+    // warning
+    else if ( pseudo ) {
+      this.reply(envelope, pseudo + ": will you paste your code on https://gist.github.com/ for fuck's sake!")
     }
 
-    this.reply(envelope, speech)
+    // help
+    else this.reply(envelope, speech)
   }
 
 })(exports)
