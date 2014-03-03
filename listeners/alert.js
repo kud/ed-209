6669e9b598f02d47a555c237a2cc19abcf26e58c
madx <madx+github@yapok.org>
2014-03-03 11:03:28 +0100

    alert: Add subscribe/unsubscribe

--- listeners/alert.js
+++ listeners/alert.js
@@ -10,17 +10,33 @@
     var str = this.util.removeCommand(message, 'alert'),
         channelData = this.client.chans[envelope.to],
         users = [],
+        mem = this.memory.namespace("alert"),
         u
 
     for (u in channelData.users) {
       if (u == 'ChanServ')       continue
       if (u == envelope.from)    continue
       if (u == this.client.nick) continue
+      if (mem.get(u) == false)   continue
       users.push(u)
     }
 
     if (str.trim().length == 0) {
       str = "wake up morons!"
+    }
+
+    if (str.trim() == "--subscribe") {
+      mem.set(envelope.from, true)
+      mem.commit()
+      this.reply(envelope, envelope.from + ": welcome back to the game!")
+      return
+    }
+
+    if (str.trim() == "--unsubscribe") {
+      mem.set(envelope.from, false)
+      mem.commit()
+      this.reply(envelope, envelope.from + ": pussy!")
+      return
     }
 
     this.reply(envelope, users.join(' '))
