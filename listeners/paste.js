c5bec55af25c7d9863c17431c3da9f8da2f6b5d6
Erwann Mest <m+github@kud.io>
2014-03-03 10:38:56 +0100

    Better paste.

--- listeners/paste.js
+++ listeners/paste.js
@@ -7,7 +7,16 @@
   }
 
   listener.callback = function(message, envelope) {
-    var speech = 'Paste your code on https://gist.github.com/ for fuck\'s sake!'
+    var args = this.util.extractParams(message, 'roll')
+      , speech
+
+    if ( typeof args[2] !== "undefined" ) {
+      speech = args[2] + ": will you paste your code on https://gist.github.com/ for fuck's sake!"
+    }
+
+    else {
+      speech = 'https://gist.github.com/'
+    }
 
     this.reply(envelope, speech)
   }
