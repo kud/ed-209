e81a821ebb57d12cef097d3d55ce394ff47748a9
Erwann Mest <m+github@kud.io>
2014-03-10 14:14:55 +0100

    Add laugh.

---
+++ listeners/laugh.js
@@ -0,0 +1,21 @@
+;(function(listener) {
+  listener.matcher = function(message, envelope) {
+    return (envelope.type == 'channel') &&
+           (message.match(/\lol\b/i) !== null ||
+           (message.match(/\ahah\b/i) !== null ||
+           (message.match(/\haha\b/i) !== null ||
+           message.match(/\mdr\b/i) !== null)
+  }
+
+  listener.callback = function(message, envelope) {
+    finalWords = [
+      "drôle !",
+      "haha",
+      "j'ai ri aussi !"
+    ],
+    finalWord = finalWords[Math.floor(Math.random() * finalWords.length)],
+
+    this.reply(envelope, finalWord)
+  }
+
+})(exports)
