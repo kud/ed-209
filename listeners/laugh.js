237c10b6f7d7735a493e9a45fe8cc1ee6be0d0c4
Erwann Mest <m+github@kud.io>
2014-03-10 14:28:07 +0100

    Another fix for laugh.

--- listeners/laugh.js
+++ listeners/laugh.js
@@ -2,13 +2,13 @@
   listener.matcher = function(message, envelope) {
     return (envelope.type == 'channel') &&
            (message.match(/\lol\b/i) !== null ||
-           (message.match(/\ahah\b/i) !== null ||
-           (message.match(/\haha\b/i) !== null ||
+           message.match(/\ahah\b/i) !== null ||
+           message.match(/\haha\b/i) !== null ||
            message.match(/\mdr\b/i) !== null)
   }
 
   listener.callback = function(message, envelope) {
-    finalWords = [
+    var finalWords = [
       "drôle !",
       "haha",
       "j'ai ri aussi !"
