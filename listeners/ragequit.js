82b9e0089e0e79d78d45e7c0f2a0b8321e00ae78
Maxime Thirouin <m@moox.io>
2015-03-12 11:32:41 +0100

    listeners/ragequit.js: coherent code style + comments

--- listeners/ragequit.js
+++ listeners/ragequit.js
@@ -9,7 +9,10 @@
   listener.callback = function(message, envelope) {
     var self = this,
         timeout = 1000 * (10 + Math.floor(Math.random() * 10)),
+        
+        // please comment each quotes
         finalWords = [
+          // kud irc quotes
           "oh et puis merde",
           "oh et puis allez tous vous faire enculer",
           "lol ok",
@@ -17,21 +20,29 @@
           "mais bordel, vos gueules",
           "en parlant de pause",
           "bon je vous quitte, la propension à juger les autres m'hérisse le poil",
-          "Tu peux modérer un peu tes propos ?", // https://github.com/putaindecode/website/issues/99#issuecomment-30603139
-          "Parfois je me demande si ce salon sert vraiment à quelque chose"
+          "Parfois je me demande si ce salon sert vraiment à quelque chose",
+          
+          // moox irc quotes
+          "bref, j'ai des kiwi à planter",
+          "bon je vais finir mon grillage :)",
+          
+          // random quotes
+          "whatever",
+          "j'vous encule",
+          "merci tout de même, bonne soirée",
+          
+          // https://github.com/putaindecode/website/issues/99#issuecomment-30603139
+          "Tu peux modérer un peu tes propos ?",
+          
           // quote de l'event "Shahor lefts", avec modification de "il a" par "il y a" pour une meilleur réutilisation
-          , "visiblement il y a un truc contre moi que je ne comprends pas bien, mais qui existe"
-          , "bref, j'ai des kiwi à planter"
-          , "whatever"
-          , "bon je vais finir mon grillage :)"
-          , "j'vous encule"
-          , "merci tout de même, bonne soirée"
+          "visiblement il y a un truc contre moi que je ne comprends pas bien, mais qui existe",
+          
         ],
         finalWord = finalWords[Math.floor(Math.random() * finalWords.length)],
         intros = [
           "vous êtes cons quand même putain",
           "re",
-          "resiously"
+          "resiously",
         ],
         intro = intros[Math.floor(Math.random() * intros.length)]
 
