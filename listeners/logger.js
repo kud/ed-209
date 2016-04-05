71bed002753514f7bd008428ac26750b2a4bbe18
François Vaux <madx@yapok.org>
2016-04-05 14:59:24 +0200

    Remove old plugins

--- listeners/logger.js
+++
@@ -1,9 +0,0 @@
-exports.matcher = function(message, envelope) {
-  var self = this
-  console.log("  " +
-              (envelope.type == 'channel' ? self.colors.yellow(envelope.to) + ' > ' : '> ') +
-              self.colors.blue(envelope.from) +
-              ': ' +
-              message)
-  return false
-}
